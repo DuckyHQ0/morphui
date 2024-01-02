ae2ed8080e307226b7816791800ed0857c7b17e1
dukc <jacobdukc@gmail.com>
2024-01-02 15:40:22 -0800

    changes

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -15,12 +15,12 @@ export const Switch = ({
 	return (
 		<div className="flex gap-8 items-center">
 			<ToggleSwitch.Root
-				className="w-64 h-36 rounded-[999px] bg-fg border border-stroke-2"
+				className="w-64 h-36 rounded-[999px] bg-fg border border-stroke-2 radix-disabled:cursor-not-allowed radix-disabled:bg-fg-disabled"
                 disabled={disabled}
                 defaultChecked={defaultChecked}
                 required={required}
 			>
-				<ToggleSwitch.Thumb className="block w-[28px] h-[28px] bg-white/5 rounded-[999px] transition-all duration-150 translate-x-[4px] will-change-transform radix-state-checked:translate-x-[30px] radix-state-checked:bg-brand-blue" />
+				<ToggleSwitch.Thumb className="block w-[28px] h-[28px] bg-white/5 radix-disabled:bg-white/[.03] rounded-[999px] transition-all ease-out duration-150 translate-x-[3px] will-change-transform radix-state-checked:translate-x-[31px] radix-state-checked:bg-brand-blue radix-disabled:radix-state-checked:bg-brand-blue/30" />
 			</ToggleSwitch.Root>
 
 			<label className="text-body text-text-1" htmlFor="c1">
